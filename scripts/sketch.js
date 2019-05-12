af7149e19b01b0ee29394da587f6f4215bcf1358
Mustafa Quraish <mustafa.quraish@mail.utoronto.ca>
2019-05-12 14:49:02 -0400

    Added modal box with info on tm2file

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -12,7 +12,7 @@ var MAX_STEPS = 1500;
 var SPACE = " ";    // Special space character to go around HTML
 
 // These are to interact with the DOM elements
-var loadBtn, oneStepBtn, allStepBtn;
+var loadBtn, oneStepBtn, allStepBtn, infBtn;
 var outText, inTM, inStr, inMaxSteps;
 
 function pad(inp, len) {
@@ -32,7 +32,7 @@ function updateText() {
     newLine += "<br> <p style='text-align:center; padding-top: 20pt; color: #a00000;'>Rejected</p>";
   }
   outText.html(oldText + "<br>" + newLine);
-  
+
   // Using native JS here instead of P5 to scroll down
   var objDiv = document.getElementById("right_div");
   objDiv.scrollTop = objDiv.scrollHeight;
@@ -79,8 +79,10 @@ function setup() {
   inTM = select("textarea");
   inTM.dragOver(() => { inTM.style('background-color', '#ccc') });
   inTM.dragLeave(() => { inTM.style('background-color', '#fff') });
-  inTM.drop((file) => { inTM.value("" + file.data); 
-                        inTM.style('background-color', '#fff') });
+  inTM.drop((file) => {
+    inTM.value("" + file.data);
+    inTM.style('background-color', '#fff')
+  });
   // Input String
   inStr = select("#tapeInput");
   inMaxSteps = select("#maxSteps");
@@ -101,6 +103,21 @@ function setup() {
   inStr.value("0000");
   inMaxSteps.value("300");
   loadInput();
+
+  // Setup Modal for info on file format
+  // Once again, using native JS here because that's
+  // what I can find online
+
+  // Get the modal
+  var modal = document.getElementById('myModal');
+  var tm2fileBtn = document.getElementById("tm2fileBtn");
+  var closeBtn = document.getElementsByClassName("close")[0];
+  tm2fileBtn.onclick = () => { modal.style.display = "block"; }
+  closeBtn.onclick = () => { modal.style.display = "none"; }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = (e) => { if (event.target == modal) modal.style.display = "none"; }
+
 }
 
 
@@ -108,17 +125,17 @@ function setup() {
 
 
 // Default TM Description
-var defaultTM = 
-`- q1 qA qR      Language:  L = {0^2^n | n >= 0}
-q1 0 q2 - R
-q2 - qA - R
-q2 0 q3 x R
-q2 x q2 x R
-q3 - q5 - L 
-q3 0 q4 0 R 
-q3 x q3 x R 
-q4 0 q3 x R 
-q4 x q4 x R 
-q5 - q2 - R 
-q5 0 q5 0 L 
+var defaultTM =
+  `- q1 qA qR       Language:  L = {0^2^n | n >= 0}
+q1 0 q2 - R    How this format works:
+q2 - qA - R    - First line consists of the symbol
+q2 0 q3 x R      used to represent the blank, the 
+q2 x q2 x R      initial state, accept state and 
+q3 - q5 - L      reject state
+q3 0 q4 0 R    - All the following lines consist  
+q3 x q3 x R      of transitions, in the same order
+q4 0 q3 x R      as transitions from Sipser
+q4 x q4 x R      state char -> state char dir 
+q5 - q2 - R    Everything after the 5th col is 
+q5 0 q5 0 L    ignored. Empty line at the end 
 q5 x q5 x L`
